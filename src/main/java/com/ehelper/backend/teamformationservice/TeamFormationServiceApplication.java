a0d969a5413c4887f6723f679535cb6c70e810fc
SkyeBeFreeman <928016560@qq.com>
2018-03-09 15:15:59 +0800

    Initial commit

---
+++ src/main/java/com/ehelper/backend/teamformationservice/TeamFormationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ehelper.backend.teamformationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFormationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFormationServiceApplication.class, args);
+	}
+}
